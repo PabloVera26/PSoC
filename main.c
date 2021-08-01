301a76f3bc0de8a19d17ef24d77dc61662699715
YYXY <58200990+adrianyyxy@users.noreply.github.com>
2021-08-01 12:50:31 -0500

    Update main.c

--- main.c
+++ main.c
@@ -8,14 +8,13 @@
 uint8 personas; //variable del contador de personas
 uint16 status0,status1; //variables para leer las entradas
 char str[9];                // variable de impresion para las personas
-
-uint sec;   //variable para contar los segundos del timer
+uint8 i;    //variable de control
 
 int main(void)
 {
+    
     CyGlobalIntEnable;
     LCD_Start();            //Inicializacion de la LCD
-    Timer_Start();  //inicializacion del timer
          
     personas=0; //iniciacion de la variable de personas
     
@@ -25,29 +24,50 @@ int main(void)
     for(;;)
     {
         
+        i=1;    //iniciar el contador de control en  1
         status0=A0_Read();  //lectura del primer sensor
         status1=A1_Read();  //lectura del segundo sensor
         
         //condicionales para leer los sensores
         //si detectamos un 0 en la entrada sumamos 1 en el numero de personas
-        
         if(status0 == 0)
         {
          
-            personas++;
-            CyDelay(250);
+            while(i==1)
+            {
+                status1=A1_Read();
                 
+                if(status1 == 0)
+                {
+         
+                    personas++;
+                    i=0;
+            
+                }
+                
+            }
+            
         }
         
         //si detectamos un 0 en la entrada restamos 1 en el numero de personas
         if(status1 == 0)
         {
          
-            personas--;
-            CyDelay(250);
+            while(i==1)
+            {
+                status0=A0_Read();
+                
+                if(status0 == 0)
+                {
+         
+                    personas--;
+                    i=0;
+            
+                }
+                
+            }
             
         }
-        
         
         //imprimir el numero de personas
         sprintf(str,"%d",personas);       //Conversion de variable float a char para su publicacion
