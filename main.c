47d3e6f74b071bdace82fd81d71a551b83e2b2eb
YayoVera26 <88364252+YayoVera26@users.noreply.github.com>
2021-08-03 18:03:06 -0500

    Update main.c

--- main.c
+++ main.c
@@ -4,6 +4,11 @@
 
 //ADRIAN PINES- SENSOR 0 EN EL PIN 2.6
 //ADRIAN PINES- SENSOR 1 EN EL PIN 2.7
+//YAYO PINES- SENSOR MQ135 0.0
+//YAYO PINES- SENSOR LM35 0.4
+//YAYO PINES- LED VERDE 0.1
+//YAYO PINES- LED AMARILLO 0.2
+//YAYO PINES- LED ROJO 0.3
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //variables adrian/contador
@@ -20,12 +25,17 @@ personas=0; //iniciacion de la variable de personas
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //variables yayo/temperatura
+int16 volts, analog2;//variable para leer adc y convertir a volts
+float Temp; //Variable de temperatura
+char str2[5]; //variable para imprimir temperatura en la LCD
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //variables yayo/co2
+int16 analog1;//variable para leer Co2
+char str3[7];//variable para imprimir en la LCD
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
@@ -95,6 +105,47 @@ int main(void)
         sprintf(str,"%d",personas);       //Conversion de variable float a char para su publicacion
         LCD_Position(3,8);
         LCD_PrintString(str);                     //Publicación del resultado en la LCD
+        
+          ////////////////////////////////////////////////// Sacar el Co2////////////////////////////////////////////////////////////////////
+       Mux_FastSelect(0); // seleccionamos canal 0 del multiplexor
+       ADC_StartConvert(); //empieza a leer el adc del canal 0
+       ADC_IsEndConversion(ADC_WAIT_FOR_RESULT); //esperamos resultado
+       analog1=ADC_GetResult16(); //recibimos resultado en 16 bits
+       ADC_StopConvert(); // paramos el adc
+       sprintf(str3,"%i",analog1); //convertimos la variable entera en char
+       LCD_Position(0,0); //posicion donde se imprimira
+       LCD_PrintString(str3); //imprimimos en LCD
+       LCD_PrintString("ppm  ");  // imprimimos el texto en lcd
+       
+       //Condiciones para los leds bueno, moderado, peligro
+       if(analog1>=0 && analog1<700){ // if para la condicion buena
+        Bueno_Write(1);
+        Moderado_Write(0);
+        Peligro_Write(0);
+    }else{
+        if(analog1>=700 && analog1<1000){//if para la condicion modearada
+            Moderado_Write(1);
+            Bueno_Write(0);
+            Peligro_Write(0);
+        }else{// condicion para el peligroso
+            Peligro_Write(1);
+            Bueno_Write(0);
+            Moderado_Write(0);
+        }
+    }
+        
+        ////////////////////////////////////////////////////////sacar temperatura////////////////////////////////////////////////////////////////
+        Mux_FastSelect(1); //seleccionamos canal 1 del multiplexor
+       ADC_StartConvert(); //empezar a convertir en el canal 2
+       ADC_IsEndConversion(ADC_WAIT_FOR_RESULT); //esperamos resultado
+       analog2=ADC_GetResult16(); //obtenemos resultado en 16bits
+       ADC_StopConvert(); // paramos el adc
+       volts=ADC_CountsTo_mVolts(analog2);// convertimos el valor leido en volts
+       Temp=((volts/10)-5);// ajustamos la conversion para la temperatura con la formula
+       sprintf(str2,"%.1f",Temp); //convertimos el valor float a char
+       LCD_Position(1,0);// posicion para imprimir
+       LCD_PrintString(str2);// imprimimos en lcs
+       LCD_PrintString("C  ");// imprimimos C en LCD
         
     }
 
